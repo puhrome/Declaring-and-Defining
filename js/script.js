fa32e66e36c79aa12b0eeda62c69e9f44c3e9383
puhrome <phirom.yim@fullsail.edu>
2014-04-13 15:39:31 -0400

    Declaring and Defining
    Using WebStorm to declare and define variables in HTML and JS.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Declaring Created by Phirom Yim on 4/13/14.
+ */
+var age; //declare making a box
+age = 31;//defining giving a value, putting in the box
+//storage container is always on the left =  value is always on the right
+
+var yearOfBirth = 1982; //declaring and defining on the same line
+//order of definition and declaration is important
+//declare before you use variable
